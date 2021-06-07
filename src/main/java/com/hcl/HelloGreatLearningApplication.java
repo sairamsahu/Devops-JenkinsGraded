8c14cd547451e5d9f06d7ef834be24706fdb98e7
sairamsahu <sairamsahu181@gmail.com>
2021-06-07 12:25:50 +0530

    first commit

---
+++ src/main/java/com/hcl/HelloGreatLearningApplication.java
@@ -0,0 +1,14 @@
+package com.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGreatLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGreatLearningApplication.class, args);
+		System.out.println("Hello Dev-ops");
+	}
+
+}
